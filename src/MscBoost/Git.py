d48e3fbd98c1cb0ad42de8a279282850051b0460
Stefan Reichör <Stefan.Reichoer@msc-technologies.eu>
2016-06-23 12:14:08 +0200

    Git.py: First step to derive GitRepository from git.Repo

--- src/MscBoost/Git.py
+++ src/MscBoost/Git.py
@@ -14,16 +14,14 @@
 
 import git
 
-class GitRepository(object):
-    def __init__(self, working_directory):
-        self.repo = git.Repo(working_directory)
+class GitRepository(git.Repo):
     def __repr__(self):
-        return "<%s '%s'>" % (self.__class__.__name__, self.repo._working_tree_dir)
+        return "<%s '%s'>" % (self.__class__.__name__, self._working_tree_dir)
     def get_branches(self):
         """
         Return a list of existing branch names for this repository
         """
-        return [b.name for b in self.repo.branches]
+        return [b.name for b in self.branches]
     def get_tags(self, commit_id=None):
         """
         Return a list of existing tag names for this repository.
@@ -31,54 +29,49 @@ class GitRepository(object):
         Otherwise return all TAGS pointing at commit_id
         """
         if commit_id is None:
-            return [t.name for t in self.repo.tags]
+            return [t.name for t in self.tags]
         else:
-            return self.repo.git.tag("--points-at", commit_id).split()
+            return self.git.tag("--points-at", commit_id).split()
     def get_commit_of_head(self):
         """
         Get the SHA-1 hash for the head commit
         """
-        return self.repo.head.commit.hexsha
+        return self.head.commit.hexsha
     def create_tag(self, tag_name):
         """
         Create a tag named tag_name at head
         """
         if tag_name not in self.get_tags():
-            self.repo.create_tag(tag_name)
+            super(self.__class__, self).create_tag(tag_name)
         return tag_name
     def create_branch(self, branch_name):
         """
         Create a branch named branch_name
         """
-        self.repo.create_head(branch_name)
+        self.create_head(branch_name)
     def push(self, with_tags=False, where_to="origin"):
         """
         Push to the remote repository
         """
         if with_tags:
-            self.repo.remotes[where_to].push("--tags")
+            self.remotes[where_to].push("--tags")
         else:
-            self.repo.remotes[where_to].push()
+            self.remotes[where_to].push()
     def pull(self):
         """
         Pull from the remote repository
         """
-        self.repo.remotes.origin.pull()
+        self.remotes.origin.pull()
     def add_remote(self, remote_url, name="origin"):
         """
         Add a remote tracking branch for remote_url. The remote is named name.
         """
-        self.repo.create_remote(name, remote_url)
-    def delete_remote(self, name):
-        """
-        Remove a remote tracking branch named name.
-        """
-        self.repo.delete_remote(name)
+        self.create_remote(name, remote_url)
     def delete_branch(self, branch_name):
         """
         Delete a branch named branch_name
         """
-        self.repo.delete_head(branch_name)
+        self.delete_head(branch_name)
 
 class MscGitRepository(GitRepository):
     def sync_to_public(self):
--- src/test/test_Git.py
+++ src/test/test_Git.py
@@ -21,15 +21,14 @@ def setup_test_repo():
     os.system("rm -fr w1")
     os.makedirs("w1")
     with Util.WorkingDirectory("w1"):
-        os.system("git init")
+        os.system("git init > /dev/null")
         os.system("touch readme.txt")
         os.system("git add readme.txt")
-        os.system("git commit -m'1st'")
+        os.system("git commit -m'1st' > /dev/null")
 
 def test_repository():
     setup_test_repo()
     g = Git.GitRepository("w1")
-    print(g)
     assert g.get_tags() == []
     assert g.get_branches() == ["master"]
     g.create_tag("root")
@@ -39,7 +38,7 @@ def test_repository():
     with Util.WorkingDirectory("w1"):
         os.system("touch readme2.txt")
         os.system("git add readme2.txt")
-        os.system("git commit -m'2nd'")
+        os.system("git commit -m'2nd' > /dev/null")
     assert g.get_tags(g.get_commit_of_head()) == []
     g.create_branch("develop")
     assert g.get_branches() == ["develop", "master"]
@@ -60,12 +59,12 @@ def test_git_remotes():
     g2 = Git.GitRepository("w2")
     g1.add_remote(os.path.join(os.getcwd(), "w2"))
     with Util.WorkingDirectory("w1"):
-        os.system("git checkout -b feature/3")
-        os.system("git push --set-upstream origin feature/3")  # Push new created branch
+        os.system("git checkout -b feature/3 &> /dev/null")
+        os.system("git push --set-upstream origin feature/3 &> /dev/null")  # Push new created branch
         os.system("git tag w1-tag")
         os.system("touch readme3.txt")
         os.system("git add readme3.txt")
-        os.system("git commit -m'3rd'")
+        os.system("git commit -m'3rd' > /dev/null")
     g1.push()
     assert g1.get_tags() == ["root", "w1-tag"]
     assert g2.get_tags() == ["root"]
@@ -75,14 +74,14 @@ def test_git_remotes():
 def test_msc_git_repository():
     m1 = Git.MscGitRepository("w1")
     with Util.WorkingDirectory("w2"):
-        os.system("git checkout feature/3")
+        os.system("git checkout feature/3 &> /dev/null")
         os.system("touch readme4.txt")
         os.system("git add readme4.txt")
-        os.system("git commit -m'4nd'")
+        os.system("git commit -m'4nd' > /dev/null")
     m1.update()
     with Util.WorkingDirectory("w1"):
         os.system("touch readme5.txt")
         os.system("git add readme5.txt")
-        os.system("git commit -m'5th'")
+        os.system("git commit -m'5th' > /dev/null")
     m1.sync_to_public()
     m1.delete_remote("origin")
