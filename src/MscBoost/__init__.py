d8ea320a56ee913791a07d3130759ea700ac7d1f
Markus Pietrek <mpie@msc-ge.com>
2016-06-29 07:58:28 +0200

    MscBoost: importing Git

--- src/MscBoost/__init__.py
+++ src/MscBoost/__init__.py
@@ -3,6 +3,7 @@
 from .Application import Application
 from .EnvironmentVariable import EnvironmentVariable
 from .FindBestMatch import FindBestMatch
+from .Git import clone, GitRepository, MscGitRepository, use_mirror
 from .UsageException import UsageException
 from .MscProject import MscProject
 from .Version import Version
