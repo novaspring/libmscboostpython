c061235527cbadfd44c5e872f00f6b380800364d
Stefan Reichör <Stefan.Reichoer@msc-technologies.eu>
2016-09-07 16:11:09 +0200

    Add MscBoost/Conversions.py

---
+++ src/MscBoost/Conversions.py
@@ -0,0 +1,83 @@
+# ----------------------------------------------------------------------------------
+#  Title      : Various conversions
+#  Project    : libMscBoostPython
+# ----------------------------------------------------------------------------------
+#  File       : Conversions.py
+#  Author     : Stefan Reichoer
+#  Company    : MSC Technologies
+#  Created    : 2016-09-07
+# ----------------------------------------------------------------------------------
+#  Description: Various conversions
+# ----------------------------------------------------------------------------------
+#  Copyright (c) 2016 -- MSC Technologies
+# ----------------------------------------------------------------------------------
+
+from MscBoost.UsageException import UsageException
+
+def int_val(value, unit="", multiplicator=1, accept_float=True):
+    if unit:
+        s_val = value[:-len(unit)]
+    else:
+        s_val = value
+    if accept_float:
+        num_val = float(s_val)
+    else:
+        num_val = int(s_val)
+    num_val *= multiplicator
+    return int(num_val)
+
+class ConversionBase(object):
+    name = None
+    def examples(self):
+        return ""  # pragma: no cover
+
+class ConvertStorageSize(ConversionBase):
+    name = "storage-size"
+    def examples(self):
+        return "1, 2B, 1.5kB, 2MB, 4GB, 1TB"
+    def convert(self, value):
+        retval = None
+        if type(value) == str:
+            if value.endswith("TB"):
+                retval = int_val(value, "TB", 1024**4)
+            elif value.endswith("GB"):
+                retval = int_val(value, "GB", 1024**3)
+            elif value.endswith("MB"):
+                retval = int_val(value, "MB", 1024**2)
+            elif value.endswith("kB"):
+                retval = int_val(value, "kB", 1024)
+            elif value.endswith("B"):
+                retval = int_val(value, "B")
+            else:
+                retval = int_val(value)
+        else:
+            retval = value
+        return retval
+
+CONVERSION_MAPPING = {}
+for obj in list(globals().values()):
+    if isinstance(obj, type) and issubclass(obj, ConversionBase):
+        if obj.name:
+            CONVERSION_MAPPING[obj.name] = obj
+
+def convert_value(value, interpretation, raise_error=False):
+    if interpretation in CONVERSION_MAPPING:
+        conv = CONVERSION_MAPPING[interpretation]()
+        try:
+            result = conv.convert(value)
+        except:
+            result = None
+            extra_info = "Examples: %s" % conv.examples()
+    else:
+        result = None
+        extra_info = "Possible interpretations: %s" % ", ".join(CONVERSION_MAPPING.keys())
+    if result is None and raise_error:
+        raise Exception("Couldn't convert '%s' as %s\n%s" % (value, interpretation, extra_info))
+    return result
+
+def convert_param_value(value, interpretation):
+    try:
+        result = convert_value(value, interpretation, raise_error=True)
+    except Exception as e:
+        raise UsageException(str(e))
+    return result
---
+++ src/test/test_Conversions.py
@@ -0,0 +1,46 @@
+# ----------------------------------------------------------------------------------
+#  Title      : Conversion tests
+#  Project    : libMscBoostPython
+# ----------------------------------------------------------------------------------
+#  File       : test_Conversions.py
+#  Author     : Stefan Reichoer
+#  Company    : MSC Technologies
+#  Created    : 2016-09-07
+# ----------------------------------------------------------------------------------
+#  Description: Conversion tests
+# ----------------------------------------------------------------------------------
+#  Copyright (c) 2016 -- MSC Technologies
+# ----------------------------------------------------------------------------------
+
+import pytest
+
+import MscBoost.Conversions as Conversions
+
+def test_conversion_basics():
+    with pytest.raises(Exception):
+        assert Conversions.int_val("15.0", accept_float=False) == 15
+    assert Conversions.int_val("15.0", accept_float=True) == 15
+    assert Conversions.int_val("15.5", accept_float=True) == 15
+    assert Conversions.int_val("15", accept_float=False) == 15
+
+def test_convert():
+    assert Conversions.convert_value(1, "dummy") is None
+    with pytest.raises(Exception):
+        assert Conversions.convert_value(1, "dummy", raise_error=True)
+    assert Conversions.convert_value("200", "storage-size") == 200
+
+def test_convert_param():
+    assert Conversions.convert_param_value("6 kB", "storage-size") == 6*1024
+    with pytest.raises(Exception):
+        assert Conversions.convert_param_value("6 kByte", "storage-size")
+
+def test_storage_size():
+    assert Conversions.convert_value(1, "storage-size") == 1
+    assert Conversions.convert_value("2", "storage-size") == 2
+    assert Conversions.convert_value("8B", "storage-size") == 8
+    assert Conversions.convert_value("1kB", "storage-size") == 1024
+    assert Conversions.convert_value("1.5kB", "storage-size") == int(1024*1.5)
+    assert Conversions.convert_value("1MB", "storage-size") == 1024*1024
+    assert Conversions.convert_value("2GB", "storage-size") == 2*1024**3
+    assert Conversions.convert_value("100TB", "storage-size") == 100*1024**4
+    assert Conversions.convert_value("100byte", "storage-size") is None
